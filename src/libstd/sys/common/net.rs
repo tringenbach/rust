04b4083ebdbfdd24506eeee3f65365fa62d04035
Steven Fackler <sfackler@gmail.com>
2015-05-26 23:47:03 -0700

    Implement RFC 1047 - socket timeouts
    Closes #25619

--- src/libstd/sys/common/net.rs
+++ src/libstd/sys/common/net.rs
@@ -20,6 +20,7 @@ use str::from_utf8;
 use sys::c;
 use sys::net::{cvt, cvt_r, cvt_gai, Socket, init, wrlen_t};
 use sys_common::{AsInner, FromInner, IntoInner};
+use time::Duration;
 
 ////////////////////////////////////////////////////////////////////////////////
 // sockaddr and misc bindings
@@ -35,16 +36,15 @@ fn setsockopt<T>(sock: &Socket, opt: c_int, val: c_int,
     }
 }
 
-#[allow(dead_code)]
 fn getsockopt<T: Copy>(sock: &Socket, opt: c_int,
                        val: c_int) -> io::Result<T> {
     unsafe {
         let mut slot: T = mem::zeroed();
         let mut len = mem::size_of::<T>() as socklen_t;
-        let ret = try!(cvt(c::getsockopt(*sock.as_inner(), opt, val,
-                                         &mut slot as *mut _ as *mut _,
-                                         &mut len)));
-        assert_eq!(ret as usize, mem::size_of::<T>());
+        try!(cvt(c::getsockopt(*sock.as_inner(), opt, val,
+                               &mut slot as *mut _ as *mut _,
+                               &mut len)));
+        assert_eq!(len as usize, mem::size_of::<T>());
         Ok(slot)
     }
 }
@@ -164,6 +164,92 @@ pub fn lookup_addr(addr: &IpAddr) -> io::Result<String> {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+// Timeouts
+////////////////////////////////////////////////////////////////////////////////
+
+#[cfg(target_os = "windows")]
+fn set_timeout(socket: &Socket, dur: Option<Duration>, kind: libc::c_int) -> io::Result<()> {
+    let timeout = match dur {
+        Some(dur) => {
+            if dur.secs() == 0 && dur.extra_nanos() == 0 {
+                return Err(io::Error::new(io::ErrorKind::InvalidInput,
+                                          "cannot set a 0 duration timeout"));
+            }
+
+            let mut timeout = if dur.secs() > (libc::DWORD::max_value() / 1000) as u64 {
+                libc::DWORD::max_value()
+            } else {
+                (dur.secs() * 1000) as libc::DWORD
+            };
+            timeout = timeout.saturating_add((dur.extra_nanos() / 1000000) as libc::DWORD);
+            if timeout == 0 {
+                timeout = 1;
+            }
+            timeout
+        }
+        None => 0
+    };
+    setsockopt(socket, libc::SOL_SOCKET, kind, timeout)
+}
+
+#[cfg(not(target_os = "windows"))]
+fn set_timeout(socket: &Socket, dur: Option<Duration>, kind: libc::c_int) -> io::Result<()> {
+    let timeout = match dur {
+        Some(dur) => {
+            if dur.secs() == 0 && dur.extra_nanos() == 0 {
+                return Err(io::Error::new(io::ErrorKind::InvalidInput,
+                                          "cannot set a 0 duration timeout"));
+            }
+
+            let secs = if dur.secs() > libc::time_t::max_value() as u64 {
+                libc::time_t::max_value()
+            } else {
+                dur.secs() as libc::time_t
+            };
+            let mut timeout = libc::timeval {
+                tv_sec: secs,
+                tv_usec: (dur.extra_nanos() / 1000) as libc::time_t,
+            };
+            if timeout.tv_sec == 0 && timeout.tv_usec == 0 {
+                timeout.tv_usec = 1;
+            }
+            timeout
+        }
+        None => {
+            libc::timeval {
+                tv_sec: 0,
+                tv_usec: 0,
+            }
+        }
+    };
+    setsockopt(socket, libc::SOL_SOCKET, kind, timeout)
+}
+
+#[cfg(target_os = "windows")]
+fn timeout(socket: &Socket, kind: libc::c_int) -> io::Result<Option<Duration>> {
+    let raw: libc::DWORD = try!(getsockopt(socket, libc::SOL_SOCKET, kind));
+    if raw == 0 {
+        Ok(None)
+    } else {
+        let secs = raw / 1000;
+        let nsec = (raw % 1000) * 1000000;
+        Ok(Some(Duration::new(secs as u64, nsec as u32)))
+    }
+}
+
+#[cfg(not(target_os = "windows"))]
+fn timeout(socket: &Socket, kind: libc::c_int) -> io::Result<Option<Duration>> {
+    let raw: libc::timeval = try!(getsockopt(socket, libc::SOL_SOCKET, kind));
+    if raw.tv_sec == 0 && raw.tv_usec == 0 {
+        Ok(None)
+    } else {
+        let sec = raw.tv_sec as u64;
+        let nsec = (raw.tv_usec as u32) * 1000;
+        Ok(Some(Duration::new(sec, nsec)))
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
 // TCP streams
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -218,6 +304,22 @@ impl TcpStream {
                   target_os = "linux")))]
     fn set_tcp_keepalive(&self, _seconds: u32) -> io::Result<()> {
         Ok(())
+    }
+
+    pub fn set_read_timeout(&self, dur: Option<Duration>) -> io::Result<()> {
+        set_timeout(&self.inner, dur, libc::SO_RCVTIMEO)
+    }
+
+    pub fn set_write_timeout(&self, dur: Option<Duration>) -> io::Result<()> {
+        set_timeout(&self.inner, dur, libc::SO_SNDTIMEO)
+    }
+
+    pub fn read_timeout(&self) -> io::Result<Option<Duration>> {
+        timeout(&self.inner, libc::SO_RCVTIMEO)
+    }
+
+    pub fn write_timeout(&self) -> io::Result<Option<Duration>> {
+        timeout(&self.inner, libc::SO_SNDTIMEO)
     }
 
     pub fn read(&self, buf: &mut [u8]) -> io::Result<usize> {
@@ -470,6 +572,22 @@ impl UdpSocket {
 
     pub fn duplicate(&self) -> io::Result<UdpSocket> {
         self.inner.duplicate().map(|s| UdpSocket { inner: s })
+    }
+
+    pub fn set_read_timeout(&self, dur: Option<Duration>) -> io::Result<()> {
+        set_timeout(&self.inner, dur, libc::SO_RCVTIMEO)
+    }
+
+    pub fn set_write_timeout(&self, dur: Option<Duration>) -> io::Result<()> {
+        set_timeout(&self.inner, dur, libc::SO_SNDTIMEO)
+    }
+
+    pub fn read_timeout(&self) -> io::Result<Option<Duration>> {
+        timeout(&self.inner, libc::SO_RCVTIMEO)
+    }
+
+    pub fn write_timeout(&self) -> io::Result<Option<Duration>> {
+        timeout(&self.inner, libc::SO_SNDTIMEO)
     }
 }
 
